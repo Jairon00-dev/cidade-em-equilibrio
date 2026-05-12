d86a77d1451d12171d23da0d4647f1beef52e502
Jairon00-dev <jaironalves085@gmail.com>
2026-05-12 12:38:35 -0300

    Primeiro Prototipo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public float dinheiro = 1000f;
+    public float felicidade = 50f;
+
+    public TextMeshProUGUI moneyText;
+    public TextMeshProUGUI HappyText;
+    public TextMeshProUGUI InforText;
+
+    void Start()
+    {
+        AtualizarUI();
+    }
+
+    public void InvestirSaude()
+    {
+        if (dinheiro >= 200)
+        {
+            dinheiro -= 200;
+            felicidade += 10;
+            InforText.text = "A saúde da cidade melhorou!";
+        }
+
+        AtualizarUI();
+    }
+
+    public void InvestirEducacao()
+    {
+        if (dinheiro >= 300)
+        {
+            dinheiro -= 300;
+            felicidade += 15;
+            InforText.text = "A educação da cidade evoluiu!";
+        }
+
+        AtualizarUI();
+    }
+
+    public void InvestirSeguranca()
+    {
+        if (dinheiro >= 250)
+        {
+            dinheiro -= 250;
+            felicidade += 12;
+            InforText.text = "A segurança aumentou!";
+        }
+
+        AtualizarUI();
+    }
+
+    void AtualizarUI()
+    {
+        moneyText.text = "Dinheiro: R$ " + dinheiro;
+        HappyText.text = "Felicidade: " + felicidade + "%";
+    }
+}
